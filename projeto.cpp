680f5089dc65acc60b5708f071b8024e415d5f24
Jorge Lucas <jorgelucas.sd@gmail.com>
2021-11-09 13:46:43 -0300

    menu²

--- projeto.cpp
+++ projeto.cpp
@@ -1,18 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 main()
 {
+    setlocale(LC_ALL, "Portuguese"); // mudar depois hehehe
 
-int opcao;
+    int opcao;
+    printf("Bem vindo a mais um software da Baianor Soluctions"); 
+    printf("\n\nEscolha o tipo de concreto desejado entre as opções abaixo"); 
+    printf("\n1 - soon..."); 
+    printf("\n2 - soon..."); 
+    printf("\n3 - soon..."); 
+    printf("\n4 - soon...");
+    printf("\n5 - Cacetada"); 
+    scanf("%d", opcao);
 
-printf("Bem vindo a mais um software da Baianor Soluctions"); 
-printf("\n Escolha o tipo de concreto desejado entre as opções abaixo"); 
-printf("Opção 1 - soon..."); 
-printf("Opção 2 - soon..."); 
-printf("Opção 3 - soon..."); 
-printf("Opção 4 - soon...");
-printf("Opção 5 - Cacetada"); 
-scanf("%d", opcao);
+    int num;
+                switch (num)// Seleção do Menu
+                 {
+                        case 1:
+                               printf("OBRIGADO\n\n");
+                               system("EXIT");
+                        break;
+                        case 2:
+                               printf("OBRIGADO\n\n");
+                               system("EXIT");
+                        break;
+                        
+                        case 3:
+                               printf("OBRIGADO\n\n");
+                               system("EXIT");
+                        break;
+                        case 4:
+                               printf("OBRIGADO\n\n");
+                               system("EXIT");
+                        break;
+                        case 5:
+                               printf("OBRIGADO\n\n");
+                               system("EXIT");
+                        break;
+                        default:
+                                printf("ok\n");
+                                } 
 }
