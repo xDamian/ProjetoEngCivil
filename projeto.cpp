44b35c0d2459d715a229bd02ebe922af40114a32
Jorge Lucas <jorgelucas.sd@gmail.com>
2021-11-09 13:30:52 -0300

    menu

--- projeto.cpp
+++ projeto.cpp
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+main()
 {
+
+int opcao;
+
 printf("Bem vindo a mais um software da Baianor Soluctions"); 
+printf("\n Escolha o tipo de concreto desejado entre as opções abaixo"); 
+printf("Opção 1 - soon..."); 
+printf("Opção 2 - soon..."); 
+printf("Opção 3 - soon..."); 
+printf("Opção 4 - soon...");
+printf("Opção 5 - Cacetada"); 
+scanf("%d", opcao);
 }
